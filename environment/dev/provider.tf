35dbdfe2b8ac58fb83f753b7003a69eea43b2663
Amit Bisht <devopstudent08@gmail.com>
2025-11-17 20:22:06 +0530

    added backend block

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -5,13 +5,12 @@ terraform {
       version = "4.49.0"
     }
   }
-  # backend "azurerm" {
-  #     resource_group_name = 
-  #     storage_account_name = 
-  #     container_name = "
-  #     key = "kjf.tfstate"
-  # }
-
+  backend "azurerm" {
+    resource_group_name  = "rglab"
+    storage_account_name = "stglactab"
+    container_name       = "cntrlab"
+    key                  = "basic.tfstate"
+  }
 }
 
 provider "azurerm" {
